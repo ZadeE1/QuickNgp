d93880b0b0ca77d2d34aa20059dfc5fd78513366
zadetomtom <tomtomzade@gmail.com>
2023-07-17 18:49:57 +1000

    added commentary

--- ngp_quick_run.bat
+++ ngp_quick_run.bat
@@ -1,21 +1,37 @@
 @echo off
 
+rem batch var is refering to the folder that this bat file is sitting in
+set batch=%~dp0
 
-set config=%~dp0config.txt
+rem makes sure the config.txt exists and has the correct path "Names" being refrenced and if it doesnt it creates a boilerplate
+set config=%batch%config.txt
+if not exists "%config%" (
+    echo config.txt doesnt exist
+    echo creating config.txt 
+)
+
+rem loads the config file
 for /f "eol=; delims=;+" %%a in (%config%) do set %%a
 
+
+rem loads conda 
 if %UseConda% == 1 (call conda activate )
 
-cd %ProjectParentDir%
+rem changes dir into the project folder which is where colmap will be run in and also making instant ngp work as intended
+cd %ProjectDir%
 
+rem checks with the user to see if they 
 :AskUseColmap
 set /p colmap_run="Use Colmap (Y/N): "
 if not "%colmap_run%" equ "N" if not "%colmap_run%" equ "Y" goto AskUseColmap
 
 
 :continue
+rem runs colmap2nerf.py only with Y is selected
 if %colmap_run% == Y (python %ColmapTwoNerf% --overwrite --run_colmap --images %images%) else (echo Not running colmap) 
+
+rem runs instant ngp, opening automatically the colmap project 
 echo Running instant ngp
-%NgpExePath% %ProjectParentDir% 
+%NgpExePath% %ProjectDir% 
 
 pause
