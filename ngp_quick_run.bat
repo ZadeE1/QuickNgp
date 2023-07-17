921be28483cc2009ebca070a17c8de980450a5f8
zadetomtom <tomtomzade@gmail.com>
2023-07-17 16:40:38 +1000

    conda

--- ngp_quick_run.bat
+++ ngp_quick_run.bat
@@ -1,10 +1,10 @@
 @echo off
 
-if %UseConda% == 1 (conda activate base)
 
 set config=%~dp0config.txt
 for /f "eol=; delims=;+" %%a in (%config%) do set %%a
 
+if %UseConda% == 1 (conda activate base)
 cd %ProjectParentDir%
 
 :AskUseColmap
