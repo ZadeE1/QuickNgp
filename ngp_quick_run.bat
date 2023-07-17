7e3854a518ab6eefdb1889a75db4bfea56c9fe62
zadetomtom <tomtomzade@gmail.com>
2023-07-17 16:25:10 +1000

    NgpQuick

---
+++ ngp_quick_run.bat
@@ -0,0 +1,18 @@
+@echo off
+
+set config=%~dp0config.txt
+for /f "eol=; delims=;+" %%a in (%config%) do set %%a
+
+cd %ProjectParentDir%
+
+:AskUseColmap
+set /p colmap_run="Use Colmap (Y/N): "
+if not "%colmap_run%" equ "N" if not "%colmap_run%" equ "Y" goto AskUseColmap
+
+
+:continue
+if %colmap_run% == Y (python %ColmapTwoNerf% --overwrite --run_colmap --images %images%) else (echo Not running colmap) 
+echo Running instant ngp
+%NgpExePath% %ProjectParentDir% 
+
+pause
