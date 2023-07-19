a0b041335867bd2c89fcd1b732cda00b8d1be69b
zadetomtom <tomtomzade@gmail.com>
2023-07-19 20:31:01 +1000

    null

--- install_rtx_3000_and_4000.bat
+++ install_rtx_3000_and_4000.bat
@@ -5,6 +5,9 @@ set batch=%~dp0
 
 cd %batch%
 
+rem using this var just because it is easier to type out 
+set short=Instant-NGP-for-RTX-3000-and-4000
+
 set config=%batch%config.txt
 
 if not exist %config% (
@@ -29,25 +32,31 @@ if exist %batch%Instant-NGP-for-RTX-3000-and-4000 (
     goto continue
 ) else (
     echo - downloading instant ngp
+    echo - #####################################################
     call certutil -urlcache -split -f https://github.com/NVlabs/instant-ngp/releases/download/continuous/Instant-NGP-for-RTX-3000-and-4000.zip instant-ngp.zip
     call tar -xvf instant-ngp.zip
-
+    echo - #####################################################
 )
 
 
 :continue
-rem loads the config file
+rem loads the config file and updates it
 echo - reconfiguring config.txt to make NgpPath to: %batch%Instant-NGP-for-RTX-3000-and-4000
 set config=%batch%config.txt
 for /f "eol=; delims=;+" %%a in (%config%) do set %%a
 
 (echo NgpPath=%batch%Instant-NGP-for-RTX-3000-and-4000 && echo ProjectDir=%ProjectDir% && echo UseConda=%UseConda%) > config.txt
 
+
 echo - attempting to install required python packages
+echo - #####################################################
+
 cd %batch%Instant-NGP-for-RTX-3000-and-4000
 call pip install -r requirements.txt
 
 cd %batch%
 
+echo - #####################################################
 echo - install completed
 pause
+exit 0
