af6ff8dfe5913e33f3316a4abf95e5c9f4b09a70
zadetomtom <tomtomzade@gmail.com>
2023-07-22 17:46:49 +1000

    option for colmap for cuda

--- install_rtx_3000_and_4000.bat
+++ install_rtx_3000_and_4000.bat
@@ -16,17 +16,21 @@ if not exist %config% (
     (echo NgpPath= && echo ProjectDir= ) > config.txt
 )
 
+:condaask
 set /p  condapause=" - use conda - recommended if you have it installed  (Y/N): "
-
 if %condapause% == Y (
     set /p condaenv=" - which conda env you would like to use?: "
     echo - activating %condaenv%
     call conda activate %condaenv%
+) else (
+    if not %condapause% == N (
+        goto condaask
+    )
 )
 
 
 
-
+rem checks if instant ngp doenst exist and installs it
 if exist %batch%Instant-NGP-for-RTX-3000-and-4000 (
     echo - instant ngp is already downloaded
     goto continue
@@ -46,22 +50,42 @@ set config=%batch%config.txt
 for /f "eol=; delims=;+" %%a in (%config%) do set %%a
 
 (echo NgpPath=%batch%Instant-NGP-for-RTX-3000-and-4000 && echo ProjectDir=%ProjectDir%) > config.txt
-
+rem loads new config vars
+for /f "eol=; delims=;+" %%a in (%config%) do set %%a
 
 echo - attempting to install required python packages
 echo - #####################################################
 
-cd %batch%Instant-NGP-for-RTX-3000-and-4000
+rem installs required python packages
+cd %NgpPath%
 call pip install -r requirements.txt
+echo - #####################################################
 
-cd %batch%
+cd %NgpPath%\scripts
 
-cd %batch%Instant-NGP-for-RTX-3000-and-4000\scripts
-call download_colmap.bat
+rem asks user if they want to install colmap for cuda
+:colmapask
+set /p colmapcuda=" - install colmap for cuda ( Y/N ): "
+if %colmapcuda% == Y (
+    echo - Downloading COLMAP for cuda...
+    call powershell -Command "(New-Object Net.WebClient).DownloadFile('https://github.com/colmap/colmap/releases/download/3.7/COLMAP-3.7-windows-cuda.zip', 'colmap.zip')"
+
+    echo - Unzipping...
+    call powershell Expand-Archive colmap.zip -DestinationPath ..\external\colmap -Force
+) else (
+    if not %colmapcuda% == N (
+        goto colmapask
+    )
+    echo - Downloading COLMAP...
+    call powershell -Command "(New-Object Net.WebClient).DownloadFile('https://github.com/colmap/colmap/releases/download/3.7/COLMAP-3.7-windows-no-cuda.zip', 'colmap.zip')"
+
+    echo - Unzipping...
+    call powershell Expand-Archive colmap.zip -DestinationPath ..\external\colmap -Force
+)
+rem installs ffmpeg
 call download_ffmpeg.bat
 
-echo - #####################################################
 echo - install completed successfully
-
+cd %batch%
 pause
 exit 0
