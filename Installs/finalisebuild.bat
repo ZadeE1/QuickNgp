22c36e5cb52eaa932cfe9514af3abdab4f9d074e
zadetomtom <tomtomzade@gmail.com>
2023-07-30 00:13:08 +1000

    added 3rd stage to selfbuilding

--- Installs/finalisebuild.bat
+++ Installs/finalisebuild.bat
@@ -1,5 +1,6 @@
 @echo off
 
+
 rem batch var is refering to the folder that this bat file is sitting in
 for %%i in ("%~dp0..") do set "folder=%%~fi"
 call :TRIM %folder% folder
@@ -7,52 +8,7 @@ set batch=%folder%\
 
 cd /d %batch%
 
-set config=%batch%config.txt
-
-if not exist %config% (
-    echo - config.txt doesnt exist
-    echo - creating config.txt 
-    (echo NgpPath= && echo ProjectDir= ) > %config%
-)
-
-rem installs instang-ngp from 
-:condaask
-if not defined condapause set /p  condapause=" - use conda - recommended if you have it installed  (Y/N): "
-    if %condapause% == Y (
-        set /p condaenv=" - which conda env you would like to use?: "
-        echo - activating %condaenv%
-        call conda activate %condaenv%
-    ) else (
-        if not %condapause% == N (
-            goto condaask
-        )
-    )
-
-
-
-echo - installing instant-ngp
-call git submodule update --init --recursive
-cd /d %batch%
-
-rem loads the config file and updates it
-echo - reconfiguring config.txt to make NgpPath to: %batch%instant-ngp
-set config=%batch%config.txt
-for /f "eol=; delims=;+" %%a in (%config%) do set %%a
-
-(echo NgpPath=%batch%Installs\instant-ngp && echo ProjectDir=%ProjectDir%) > %config%
-rem loads new config vars
-for /f "eol=; delims=;+" %%a in (%config%) do set %%a
-
-CALL :TRIM %NgpPath% NgpPath
-echo %NgpPath%
-cd /d %batch%Installs\instant-ngp
-
-
-call git submodule update --init --recursive
-pip install -r requirements.txt
-
-
-
+rem asks user if they want to install colmap for cuda
 :colmapask
 
 rem if colmap has not been declared using the cmdline arguments it asks the user
@@ -81,11 +37,3 @@ if %colmapcuda% == Y (
 
 rem installs ffmpeg
 call download_ffmpeg.bat
-
-
-pause
-exit 0
-
-:TRIM
-SET %2=%1
-GOTO :EOF
